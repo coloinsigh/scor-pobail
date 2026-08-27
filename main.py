b50cdcc4f37739451f8df3eb137962566d8599b7
coloinsigh <coulomb.lynch@gmail.com>
2026-08-27 21:37:11 +0100

    Implement Dijkstra algorithm for minimizing path between nodes

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import osmnx as ox
 import geopandas as gp
-import time
-
-print(f"OSMNX vesion {ox.__version__}")
-print(f"Geopandas vesion {gp.__version__}")
+import networkx
 
 LOCATION = "Galway, Ireland"    # central point
 DISTANCE = 10000.0              # radius of circle in m
+SPEED = 5*1000/3600             # assumed walking speed - 5km/h
 
 # Fetch initial graph, centred at a specific location
 g = ox.graph_from_address(LOCATION, DISTANCE, dist_type="bbox", network_type="walk", retain_all=True)
@@ -18,8 +16,18 @@ pitches = ox.features_from_address(LOCATION, tags={'leisure': 'pitch'}, dist=DIS
 nearest_node = []
 for ind in range(len(pitches)):
     # iterate across all amenities and find nearest node
-    p_loc = pitches.iloc[ind][0].centroid
+    p_loc = pitches.iloc[ind].iloc[0].centroid
 
     nearest_node.append(ox.nearest_nodes(g, p_loc.x, p_loc.y))
 # Handle deduplication of repeated nodes
 nearest_node_dedup = set(nearest_node)
+
+# Next, need to calculate the nearest node to every amenity
+nearest = networkx.multi_source_dijkstra_path_length(g, sources=nearest_node_dedup, weight="length")
+
+# Unpack nodes into a geodataframe
+gdf_nodes = ox.graph_from_gdfs(g, edges=False)
+gdf_nodes['pitch_dist_m'] = gdf_nodes.index.map(nearest)
+
+# Now convert the metres to minutes, based on reasonable walking speed
+gdf_nodes['pitch_walk_min'] = gdf_nodes['pitch_dist_m']/SPEED
